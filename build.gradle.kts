59ec0b821a6bffcbbae576ab141f3aa860c910cc
Aravind0211 <113758941+Aravind0211@users.noreply.github.com>
2024-05-06 15:47:11 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,10 @@ plugins {
 repositories {
     mavenCentral()
 }
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
