74f7bc4cf1791af99f55af942514d58aaf76b347
Aravind0211 <113758941+Aravind0211@users.noreply.github.com>
2024-05-06 15:20:24 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,45 @@
 plugins {
-    id("java")
+    id 'java'
+    id 'org.springframework.boot' version '2.7.7'
+    id 'io.spring.dependency-management' version '1.0.15.RELEASE'
+    id 'org.sonarqube' version '2.7'
+    id 'org.cyclonedx.bom' version '1.8.2'
 }
-
+apply plugin: 'java'
+apply plugin: 'maven-publish'
+group = 'com.logicfocus'
+version = '2.0'
+sourceCompatibility = 1.8 // java 8
+targetCompatibility = 1.8
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
-
+cyclonedxBom {
+ includeConfigs = ["runtimeClasspath"]
+ projectType = "application"
+ schemaVersion = "1.5"
+ destination = file("build/reports")
+ outputName = "bom"
+ outputFormat = "xml"
+ includeBomSerialNumber = false
+ includeLicenseText = false
+ componentVersion = "2.0.0"
+}
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  implementation 'com.google.guava:guava:29.0-jre'
 }
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+	withSourcesJar()
+	withJavadocJar()
+}
+publishing {
+    publications {
+        maven(MavenPublication) {
+		from components.java
+        }
     }
+}
+wrapper {
+    gradleVersion = "7.3.3"
+    distributionType = Wrapper.DistributionType.ALL
 }
